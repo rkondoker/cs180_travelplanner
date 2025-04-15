2621984f17b95eb45f57baa26b63a3e3030bf170
j1munoz <jimmymunoz397@gmail.com>
2025-04-14 19:23:25 -0700

    Add initial files

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Example from "@/components/example.js";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      Hello World
+      <Example />
+    </div>
+  );
+}
+
+export default Home;
