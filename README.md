5fadef9206a00829a759477cb846bed084724298
j1munoz <jimmymunoz397@gmail.com>
2025-04-14 22:25:13 -0700

    Prettier extension

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # CS180_TravelPlanner
 
 ## Commands
+
 ### Pulling From Repository
+
 When you pull the repoistory to your local machine, be sure to run the following command to install all dependencies:
+
 ```
 npm i
 ```
 
 ### Running the Website
-WHen you want to run the website, use the following command:
+
+When you want to run the website, use the following command:
+
 ```
 npm run dev
 ```
--- src/components/example.js
+++ src/components/example.js
@@ -1,11 +1,17 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Example = () => {
-    return (
-        <div className="flex flex-col items-center justify-center">
-            <Link href="https://github.com/rkondoker/CS180_TravelPlanner" className="bg-red-400 p-4 rounded-full" target="_blank">Our Repository</Link>
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Link
+        href="https://github.com/rkondoker/CS180_TravelPlanner"
+        className="bg-red-400 p-4 rounded-full"
+        target="_blank"
+      >
+        Our Repository
+      </Link>
+    </div>
+  );
+};
 
 export default Example;
