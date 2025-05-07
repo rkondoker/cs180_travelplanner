9572b893e7dd50605a4c22f4b7ca1b47382a53ac
IhsanSarwar <sarwa@IhsanLaptop>
2025-05-07 16:46:46 -0700

    Fixed Errors Listed in previous pull request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-"use client";
-
 import Example from "@/components/example";
 import LogIn from "@/components/login";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "@/components/Navbar";
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <Navbar />
       <Example />
       <LogIn />
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,39 @@
 "use client";
 
-import { Link } from "react-router-dom";
+import Link from "next/link";
 
 const Navbar = () => {
-    return (
-        <nav className="bg-trip-brown-200 p-6 -xl w-full shadow-lg font-trip-main"> 
-            <div className="flex justify-between items-center">
-               
-           <div> 
-            <a href = "/" className= "text-amber--200 text-2xl hover:text-gray-300">TripWise</a>
-           </div>
-           <div className="flex space-x-6"> 
-            <a href = "/about" className= "text-amber-100 text-lg hover:text-gray-300">Trip Planner</a>
-            <a href = "/contact" className= "text-amber-100 text-lg hover:text-gray-300">My Trips</a>
-           </div>
-        
-            {/*<h1>Sample Navigation Bar to Upload to Homepage once devloped
-            </h1>*/}
+  return (
+    <nav className="bg-trip-brown-200 p-6 -xl w-full shadow-lg font-trip-main">
+      <div className="flex justify-between items-center">
+        <div>
+          <Link
+            href="/"
+            className="text-amber--200 text-2xl hover:text-gray-300"
+          >
+            TripWise
+          </Link>
         </div>
-        </nav>        
-    )
+        <div className="flex space-x-6">
+          <Link
+            href="/trip-planner"
+            className="text-amber-100 text-lg hover:text-gray-300"
+          >
+            Trip Planner
+          </Link>
+          <Link
+            href="/my-trips"
+            className="text-amber-100 text-lg hover:text-gray-300"
+          >
+            My Trips
+          </Link>
+        </div>
 
-}
+        {/*<h1>Sample Navigation Bar to Upload to Homepage once devloped
+            </h1>*/}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
