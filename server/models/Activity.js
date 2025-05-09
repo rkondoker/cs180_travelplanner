62dcdcf0339f2cb3e1b13ac149593a36aa9530bb
j1munoz <jimmymunoz397@gmail.com>
2025-05-09 16:15:33 -0700

    Take #3

--- server/models/Activity.js
+++ server/models/Activity.js
@@ -1,28 +1,37 @@
 class Activity {
-    constructor(activityId, tripId, name, location, category, startDate, endDate, dateCreated) {
-        this.activityId = activityId;
-        this.tripId = tripId;
-        this.name = name;
-        this.location = location;
-        this.category = category;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.dateCreated = dateCreated;
-    }
+  constructor(
+    activityId,
+    tripId,
+    name,
+    location,
+    category,
+    startDate,
+    endDate,
+    dateCreated,
+  ) {
+    this.activityId = activityId;
+    this.tripId = tripId;
+    this.name = name;
+    this.location = location;
+    this.category = category;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.dateCreated = dateCreated;
+  }
 
-    // Static method to create an activity from database row
-    static fromDbRow(row) {
-        return new Activity(
-            row.activity_id,
-            row.trip_id,
-            row.name,
-            row.location,
-            row.category,
-            row.start_date,
-            row.end_date,
-            row.date_created
-        );
-    }
+  // Static method to create an activity from database row
+  static fromDbRow(row) {
+    return new Activity(
+      row.activity_id,
+      row.trip_id,
+      row.name,
+      row.location,
+      row.category,
+      row.start_date,
+      row.end_date,
+      row.date_created,
+    );
+  }
 }
 
 module.exports = Activity;
