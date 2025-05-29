89d0f52da25bf7a66b759eed07f8a31fb2a22d17
Tegh <tegh@Teghs-MacBook-Pro.local>
2025-05-28 19:46:55 -0700

    AI integration

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   (
